761d0abe9f85e854566b07e7d42765fefcee8062
Dipshirsha <140639876+Dipshirsha@users.noreply.github.com>
2024-06-26 19:41:12 +0530

    Update songRoute.js

--- Routes/songRoute.js
+++ Routes/songRoute.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const songData = require("../Schema/songDataModel");
+const mongoose = require('mongoose');
+
+
+
+
+async function fetchDocumentsByIds(ids,page) {
+  try {
+    // Convert string IDs to ObjectId if necessary
+  
+
+    // Fetch documents using the $in operator
+    const skip = (page - 1) * 5;
+    
+    // Fetch documents using the $in operator with pagination
+    const documents = await songData.find({ _id: { $in: ids } })
+                                    .skip(skip)
+                                    .limit(5);
+
+
+    console.log(documents);
+    return documents;
+  } catch (error) {
+    console.error('Error fetching documents:', error);
+  } 
+}
 
 
 router.post("/addsong", async (req, res) => {
@@ -18,7 +43,7 @@ router.post("/addsong", async (req, res) => {
         albumName: albumName,
         likes: 0,
         language: language,
-        date:date,
+        date:new Date(date),
         plays:0,
         
       });
@@ -32,15 +57,12 @@ router.post("/addsong", async (req, res) => {
 
   router.get("/song", async (req, res) => {
     const page = parseInt(req.query.page) || 1;
- 
+    
+    const limit=50;
+  
     try {
-      const allSong = await songData.find();
-
-      const limit = 30;
-      const startIndex = (page - 1) * limit;
-      const endIndex = page * limit;
-      const resultItems = allSong.slice(startIndex, endIndex);
-      res.status(200).json(resultItems);
+      const allSong = await songData.find().skip((page - 1) * limit).limit(limit);
+      res.status(200).json(allSong);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -48,9 +70,13 @@ router.post("/addsong", async (req, res) => {
 
 router.post("/fetchplaylist", async (req, res) => {
   try {
-    const {id} = req.body;
-    const singleSong = await songData.findById(id);
-    res.status(200).json(singleSong);
+
+
+
+
+    const {id,page} = req.body;
+    const song = await fetchDocumentsByIds(id,page)
+    res.status(200).json(song);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -86,9 +112,9 @@ router.post("/updateplays", async (req, res) => {
 
 router.post("/fetchhistory", async (req, res) => {
   try {
-    const {id} = req.body;
-    const singleSong = await songData.findById(id);
-    res.status(200).json(singleSong);
+    const {id,page} = req.body;
+    const song = await fetchDocumentsByIds(id,page)
+    res.status(200).json(song);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -97,9 +123,9 @@ router.post("/fetchhistory", async (req, res) => {
 
 router.post("/fetchliked", async (req, res) => {
   try {
-    const {id} = req.body;
-    const singleSong = await songData.findById(id);
-    res.status(200).json(singleSong);
+    const {id,page} = req.body;
+    const song = await fetchDocumentsByIds(id,page)
+    res.status(200).json(song);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -117,6 +143,109 @@ router.post("/fetchliked", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 }); 
+
+
+
+router.post("/fetchsingersong", async (req, res) => {
+  try {
+    const {id,page} = req.body;
+    const song = await fetchDocumentsByIds(id,page)
+    res.status(200).json(song);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.post("/fetchalbumsong", async (req, res) => {
+  try {
+    const {id,page} = req.body;
+    const song = await fetchDocumentsByIds(id,page)
+    res.status(200).json(song);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/fresh", async (req, res) => {
+  try {
+
+    const currentDate = new Date(); // Current date
+    const twentyDaysAgo = new Date();
+    twentyDaysAgo.setDate(currentDate.getDate() - 20);
+    const {page} = req.body ;
+    const skip = (page - 1) * 5;
+
+
+    // Query MongoDB for songs under 20 days old with pagination
+    const recentSongs = await songData.find({ date: { $gte: twentyDaysAgo, $lte: currentDate } })
+        .skip(skip)
+        .limit(5)
+
+      res.status(200).json(recentSongs);
+      console.log(recentSongs)
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/top", async (req, res) => {
+  try {
+
+
+    const {page} = req.body ;
+    const skip = (page - 1) * 5;
+
+
+    // Query MongoDB for songs under 20 days old with pagination
+    const recentSongs = await songData.find({ likes: { $gte: 5 } })
+        .skip(skip)
+        .limit(5)
+
+      res.status(200).json(recentSongs);
+      console.log(recentSongs)
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/trend", async (req, res) => {
+  try {
+
+
+    const {page} = req.body ;
+    const skip = (page - 1) * 5;
+
+
+    // Query MongoDB for songs under 20 days old with pagination
+    const recentSongs = await songData.find({ likes: { $gte: 5 },plays:{ $gte: 10} })
+        .skip(skip)
+        .limit(5)
+
+      res.status(200).json(recentSongs);
+      console.log(recentSongs)
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/filter-songs', async (req, res) => {
+  try {
+    const { type, language } = req.body;
+
+    const filteredSongs = await songData.find({
+      type: { $in: [new RegExp(type, 'i')] },
+      language: new RegExp(language, 'i')
+    });
+
+    res.json(filteredSongs);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while fetching songs' });
+  }
+});
 
 
 
